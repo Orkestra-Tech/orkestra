96cdf187c3e3a46bc0d1cc0a66b99600817ca828
Joan <joan@goyeau.com>
2017-07-07 11:28:51 +0100

    Keep the scroll down on logs

--- orchestra/src/main/scala/com/goyeau/orchestra/pages/LogsPage.scala
+++ orchestra/src/main/scala/com/goyeau/orchestra/pages/LogsPage.scala
@@ -10,6 +10,7 @@ import com.goyeau.orchestra.routes.WebRouter.TaskLogsPage
 import japgolly.scalajs.react._
 import japgolly.scalajs.react.component.builder.Lifecycle.ComponentDidMount
 import japgolly.scalajs.react.vdom.html_<^._
+import org.scalajs.dom.{document, window}
 
 object LogsPage {
   case class Props(page: TaskLogsPage)(implicit val ec: ExecutionContext)
@@ -25,19 +26,21 @@ object LogsPage {
         )
       }
       .componentDidMount { $ =>
-        implicit val ec = $.props.ec
-
-        def pullLogs($ : ComponentDidMount[Props, (String, Int, SetIntervalHandle), Unit]) =
-          $.props.page.job.Api.client
-            .logs($.props.page.runId, $.state._2)
-            .call()
-            .foreach { logs =>
-              $.modState(_.copy(_1 = $.state._1 + logs.mkString("\n"), _2 = $.state._2 + logs.size)).runNow()
-            }
-
         pullLogs($)
         $.setState($.state.copy(_3 = js.timers.setInterval(1.second)(pullLogs($))))
       }
       .componentWillUnmount($ => Callback(js.timers.clearInterval($.state._3)))
       .build
+
+  private def pullLogs($ : ComponentDidMount[Props, (String, Int, SetIntervalHandle), Unit]) = {
+    implicit val ec = $.props.ec
+    $.props.page.job.Api.client
+      .logs($.props.page.runId, $.state._2)
+      .call()
+      .foreach { logs =>
+        val isScrolledToBottom = window.innerHeight + window.pageYOffset + 1 >= document.body.scrollHeight
+        $.modState(_.copy(_1 = $.state._1 + logs.mkString("\n"), _2 = $.state._2 + logs.size)).runNow()
+        if (isScrolledToBottom) window.scrollTo(0, document.body.scrollHeight)
+      }
+  }
 }
