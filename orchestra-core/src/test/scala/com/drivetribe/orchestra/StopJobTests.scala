1a013ef792a82ef10ff6c1d4e5e9793a241e70d1
Joan Goyeau <joan@goyeau.com>
2018-04-11 16:17:54 +0100

    Add test from stopping a job

---
+++ orchestra-core/src/test/scala/com/drivetribe/orchestra/StopJobTests.scala
@@ -0,0 +1,31 @@
+package com.drivetribe.orchestra
+
+import com.drivetribe.orchestra.model.RunId
+import com.drivetribe.orchestra.utils.DummyJobs._
+import com.drivetribe.orchestra.utils._
+import org.scalatest.Matchers._
+import org.scalatest.concurrent.Eventually
+import shapeless.HNil
+
+class StopJobTests
+    extends OrchestraSpec
+    with OrchestraConfigTest
+    with KubernetesTest
+    with ElasticsearchTest
+    with Eventually {
+
+  scenario("Stop a job") {
+    val runId = RunId.random()
+    emptyJobRunner.ApiServer().trigger(runId, HNil).futureValue
+    eventually {
+      val runningJobs = CommonApiServer().runningJobs().futureValue
+      runningJobs should have size 1
+    }
+
+    emptyJobRunner.ApiServer().stop(runId).futureValue
+    eventually {
+      val runningJobs = CommonApiServer().runningJobs().futureValue
+      runningJobs should have size 0
+    }
+  }
+}
